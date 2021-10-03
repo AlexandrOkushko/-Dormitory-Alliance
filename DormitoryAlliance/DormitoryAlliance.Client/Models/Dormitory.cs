b0b4887f4de34dca765d36bed51b978034b25f0a
AlexandrOkushko <sevenr23@gmail.com>
2021-10-03 18:05:32 +0300

    add: models

---
+++ DormitoryAlliance/DormitoryAlliance.Client/Models/Dormitory.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DormitoryAlliance.Client.Models
+{
+    [Table("Dormitories")]
+    public class Dormitory
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, Column(TypeName = "nvarchar(100)")]
+        public string Address { get; set; }
+
+        [Required]
+        public int Floors { get; set; }
+    }
+}
