8ea6c174977ec7343cfeb795c271d6a28b8564a1
AlexandrOkushko <sevenr23@gmail.com>
2021-10-03 18:12:12 +0300

    add: DbContext.cs

---
+++ DormitoryAlliance/DormitoryAlliance.Client/Models/DormitoryAllianceDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DormitoryAlliance.Client.Models
+{
+    public class DormitoryAllianceDbContext : DbContext
+    {
+        public DormitoryAllianceDbContext(DbContextOptions<DormitoryAllianceDbContext> options) : base(options) { }
+
+        public DbSet<Dormitory> Dormitories { get; set; }
+        public DbSet<Group> Groups { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
