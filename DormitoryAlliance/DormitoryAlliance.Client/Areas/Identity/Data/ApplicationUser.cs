4fab0a1740315dc49997a708babc8bdca4b6b0c4
AlexandrOkushko <sevenr23@gmail.com>
2021-10-10 21:32:23 +0300

    add: user table and role

--- DormitoryAlliance/DormitoryAlliance.Client/Areas/Identity/Data/ApplicationUser.cs
+++ DormitoryAlliance/DormitoryAlliance.Client/Areas/Identity/Data/ApplicationUser.cs
@@ -1,14 +1,25 @@
-using System;
-using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 
 namespace DormitoryAlliance.Client.Areas.Identity.Data
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
+    [Table("Users")]
     public class ApplicationUser : IdentityUser
     {
+        [PersonalData, Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string FirstName { get; set; }
+        
+        [PersonalData, Required]
+        [Column(TypeName = "nvarchar(100)")]
+        public string LastName { get; set; }
+        
+        [Required]
+        public int RoleId { get; set; }
+
+        [ForeignKey("RoleId")]
+        public Role Role { get; set; }
     }
 }
