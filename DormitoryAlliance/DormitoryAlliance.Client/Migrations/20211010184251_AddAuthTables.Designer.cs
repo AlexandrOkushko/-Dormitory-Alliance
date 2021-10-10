17f96785ee50a7e349434d06315b5d044c4d0f2b
AlexandrOkushko <sevenr23@gmail.com>
2021-10-10 21:47:40 +0300

    add: ef migration (dotnet ef migrations add AddAuthTables)

--- DormitoryAlliance/DormitoryAlliance.Client/Migrations/20211010184251_AddAuthTables.Designer.cs
+++ DormitoryAlliance/DormitoryAlliance.Client/Migrations/20211010184251_AddAuthTables.Designer.cs
@@ -4,14 +4,16 @@ using DormitoryAlliance.Client.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DormitoryAlliance.Client.Migrations
 {
     [DbContext(typeof(DormitoryAllianceDbContext))]
-    partial class DormitoryAllianceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20211010184251_AddAuthTables")]
+    partial class AddAuthTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
