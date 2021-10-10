da82fdbf138c94120ff4d34fefd9785383c75d7a
AlexandrOkushko <sevenr23@gmail.com>
2021-10-10 19:00:48 +0300

    add: identity

---
+++ DormitoryAlliance/DormitoryAlliance.Client/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+using System;
+using DormitoryAlliance.Client.Areas.Identity.Data;
+using DormitoryAlliance.Client.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(DormitoryAlliance.Client.Areas.Identity.IdentityHostingStartup))]
+namespace DormitoryAlliance.Client.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AuthDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("DormitoryAllianceConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<AuthDbContext>();
+            });
+        }
+    }
+}
