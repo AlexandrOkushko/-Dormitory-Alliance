c4b74b7476e5e38a3ddf02ea8127ec2dfaee3f88
AlexandrOkushko <sevenr23@gmail.com>
2021-11-02 16:37:32 +0200

    add: new migration initial

--- DormitoryAlliance/DormitoryAlliance.Client/Migrations/20211025081844_Initial.cs
+++ DormitoryAlliance/DormitoryAlliance.Client/Migrations/20211025081844_Initial.cs
@@ -34,6 +34,26 @@ namespace DormitoryAlliance.Client.Migrations
                 });
 
             migrationBuilder.CreateTable(
+                name: "Rooms",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Number = table.Column<int>(type: "int", nullable: false),
+                    DormitoryId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rooms", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Rooms_Dormitories_DormitoryId",
+                        column: x => x.DormitoryId,
+                        principalTable: "Dormitories",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
                 name: "Students",
                 columns: table => new
                 {
@@ -42,8 +62,7 @@ namespace DormitoryAlliance.Client.Migrations
                     Name = table.Column<string>(type: "nvarchar(30)", nullable: false),
                     Surname = table.Column<string>(type: "nvarchar(30)", nullable: false),
                     Patronymic = table.Column<string>(type: "nvarchar(30)", nullable: true),
-                    Room = table.Column<int>(type: "int", nullable: false),
-                    DormitoryId = table.Column<int>(type: "int", nullable: false),
+                    RoomId = table.Column<int>(type: "int", nullable: false),
                     GroupId = table.Column<int>(type: "int", nullable: false),
                     Course = table.Column<int>(type: "int", nullable: false)
                 },
@@ -51,28 +70,33 @@ namespace DormitoryAlliance.Client.Migrations
                 {
                     table.PrimaryKey("PK_Students", x => x.Id);
                     table.ForeignKey(
-                        name: "FK_Students_Dormitories_DormitoryId",
-                        column: x => x.DormitoryId,
-                        principalTable: "Dormitories",
-                        principalColumn: "Id",
-                        onDelete: ReferentialAction.Cascade);
-                    table.ForeignKey(
                         name: "FK_Students_Groups_GroupId",
                         column: x => x.GroupId,
                         principalTable: "Groups",
                         principalColumn: "Id",
                         onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Students_Rooms_RoomId",
+                        column: x => x.RoomId,
+                        principalTable: "Rooms",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
                 });
 
             migrationBuilder.CreateIndex(
-                name: "IX_Students_DormitoryId",
-                table: "Students",
+                name: "IX_Rooms_DormitoryId",
+                table: "Rooms",
                 column: "DormitoryId");
 
             migrationBuilder.CreateIndex(
                 name: "IX_Students_GroupId",
                 table: "Students",
                 column: "GroupId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_RoomId",
+                table: "Students",
+                column: "RoomId");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
@@ -81,10 +105,13 @@ namespace DormitoryAlliance.Client.Migrations
                 name: "Students");
 
             migrationBuilder.DropTable(
-                name: "Dormitories");
+                name: "Groups");
 
             migrationBuilder.DropTable(
-                name: "Groups");
+                name: "Rooms");
+
+            migrationBuilder.DropTable(
+                name: "Dormitories");
         }
     }
 }
